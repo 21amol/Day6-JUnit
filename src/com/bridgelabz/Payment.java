3bba6efd844574871b12276086593ff141a94408
Amol Ghotale <ghotaleamol@gmail.com>
2022-03-24 13:17:19 +0530

    MODIFIED: Payments Main Method

--- src/com/bridgelabz/Payment.java
+++ src/com/bridgelabz/Payment.java
@@ -2,12 +2,7 @@ package com.bridgelabz;
 
 public class Payment {
 
-
-    static void monthlyPayment() {
-
-
-        // using command line arguments.
-        String[] args = new String[0];
+    public static void main(String[] args) {
         int principal = Integer.parseInt(args[0]);          // Principal Amount.
         int years = Integer.parseInt(args[1]);              // Years.
         double rate = Integer.parseInt(args[2]);               // Rate of Interest.
@@ -18,10 +13,5 @@ public class Payment {
 
         double payments = ((principal * r) / 1 - (a));
         System.out.println("The Monthly Payment is: " + payments);
-
-    }
-
-    public static void main(String[] args) {
-        Payment.monthlyPayment();
     }
 }
